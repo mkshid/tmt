b5dbbf96e589f9074f269bab70453efd07aae186
M. Hassan Khurshid <mkshid@gmail.com>
2017-10-18 23:53:14 +0200

    Add nav bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import { Grid } from 'react-bootstrap';
 import './App.css';
+import NavBar from './Navbar';
 
 class App extends Component {
   render() {
     return (
-      <Grid className='main-container'>
-        <p> Hello there</p>
-      </Grid>
+      <div>
+        <NavBar />
+        <Grid className='main-container'>
+          <h1 > Hey, looking for a new <u>serie</u>? </h1>
+          <h3 className='first-question'>
+            How many <b> minutes  </b> do you have? </h3>
+        </Grid>
+      </div>
     );
   }
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  Nav, Navbar, NavItem
+} from 'react-bootstrap';
+
+
+const NavBar = (props) => {
+  
+  return (
+    <Navbar inverse collapseOnSelect>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <a href="#">TmT (Take my Time)</a>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+      </Navbar.Header>
+      <Navbar.Collapse>
+        <Nav pullRight>
+          <NavItem eventKey={1} href="#">Tv</NavItem>
+          <NavItem eventKey={2} href="#">About</NavItem>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+export default NavBar;
