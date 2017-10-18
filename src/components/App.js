782eff18f8dd1d22326da7353176c9b328f2c773
M. Hassan Khurshid <mkshid@gmail.com>
2017-10-18 23:28:15 +0200

    Inital commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Grid } from 'react-bootstrap';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Grid className='main-container'>
+        <p> Hello there</p>
+      </Grid>
+    );
+  }
+}
+
+export default App;
