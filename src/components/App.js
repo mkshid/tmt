d97aeed9006471d7c4f98c482725bccffc7d3dd5
M. Hassan Khurshid <mkshid@gmail.com>
2017-12-13 23:02:03 +0100

    Add dummy Results component

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import './App.css';
 import NavBar from './Navbar';
 import Selector from './Selector';
+import Results from './Results';
+import {isEmpty as l_isEmpty} from 'lodash';
 import { API_KEY as api_key, BASE_DISCOVER_URL } from '../settings';
 
 class App extends Component {
@@ -16,7 +18,7 @@ class App extends Component {
         1: {text: '10-20', gte: 10, lte: 20},
         2: {text: '20-30', gte: 20, lte: 30},
         3: {text: '30-40', gte: 40, lte: 50},
-        4: {text: '50-60', gte: 50, lte: 60}
+        4: {text: 'More', gte: 50}
       },
       selected: -1,
       data: {}
@@ -39,13 +41,21 @@ class App extends Component {
   }
 
   render() {
+    let component_to_view = (
+      <Selector
+         times={this.state.times}
+         handleClick={this.handleClick.bind(this)}
+         />
+    );
+
+    if (!l_isEmpty(this.state.data)){
+      component_to_view = <Results />;
+    }
+
     return (
       <div>
         <NavBar />
-        <Selector
-           times={this.state.times}
-           handleClick={this.handleClick.bind(this)}
-           />
+        {component_to_view}
       </div>
     );
   }
---
+++ src/components/Results.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Results = (props) => {
+
+  return (<div>here are the requests!</div>);
+
+}
+
+export default Results;
