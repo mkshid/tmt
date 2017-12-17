7f35eb40bede6e9add47318da5804840ab1f70e4
M. Hassan Khurshid <mkshid@gmail.com>
2017-12-17 16:14:08 +0100

    Improve Results component style

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,12 +1,17 @@
+import './Results.css';
 import React from 'react';
 import { Grid } from 'react-bootstrap';
 
 const Results = ({data}) => {
 
-  const results = data.results.map((film) => (
+  const reduced_results = data.results.slice(0, 8);
+  const results = reduced_results.map((film) => (
     <div className='film' key={film.id}>
-      <p>{film.vote_average}/10</p>
-      <p>{film.title}</p>
+      <div className='info'>
+        <div className='vote'>{film.vote_average}/10</div>
+        <div className='title'>{film.title}</div>
+      </div>
+      <img src={`https://image.tmdb.org/t/p/w500${film.poster_path}`}/>
     </div>
   ));
 
