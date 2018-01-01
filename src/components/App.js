9bd3bc0649f750fcc264237e671e7cd26bd83a73
M. Hassan Khurshid <mkshid@gmail.com>
2018-01-01 23:27:35 +0100

    Add redux, react-redux skeleton

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import {
   Route, Switch, Redirect
 } from 'react-router-dom';
 
+
 import './App.css';
 
 import NavBar from './Navbar';
--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 import './index.css';
 import App from './components/App';
+import reducers from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = createStore(
+  reducers,
+  composeWithDevTools(
+    applyMiddleware(reduxThunk)
+  )
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+    , document.getElementById('root'));
 registerServiceWorker();
